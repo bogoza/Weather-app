766e77a022c9bd2dc771a50d1054858a43529d55
chach <chachanidze.grg@gmail.com>
2023-06-09 19:31:07 +0400

    First commit

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.weatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.weatherapp.databinding.ActivityMainBinding
+import com.example.weatherapp.fragments.MainFragment
+import org.json.JSONObject
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        goToMainFrag()
+    }
+
+    private fun goToMainFrag(){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.placeHolder,MainFragment.newInstance())
+            .commit()
+    }
+}
---
+++ app/src/main/java/com/example/weatherapp/fragments/MainFragment.kt
@@ -0,0 +1,25 @@
+package com.example.weatherapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.weatherapp.R
+import com.example.weatherapp.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+    private var _binding:FragmentMainBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+    companion object{
+        @JvmStatic
+        fun newInstance() = MainFragment()
+    }
+}
