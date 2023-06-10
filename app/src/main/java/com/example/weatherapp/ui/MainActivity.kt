ce44e05d7271770e0069ff415fdb340ad213a237
chach <chachanidze.grg@gmail.com>
2023-06-10 18:06:04 +0400

    fixed ui

--- app/src/main/java/com/example/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/ui/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.weatherapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.view.Window
+import android.view.WindowManager
+import androidx.cardview.widget.CardView
+import androidx.core.view.ViewCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.weatherapp.R
@@ -13,19 +18,21 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navHosTFragment = supportFragmentManager
             .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         navController = navHosTFragment.navController
+
+
+
         navController.navigate(R.id.mainFragment)
+
+
     }
 
-//    private fun goToMainFrag(){
-//        supportFragmentManager
-//            .beginTransaction()
-//            .replace(R.id.placeHolder,MainFragment.newInstance())
-//            .commit()
-//    }
+
 }
