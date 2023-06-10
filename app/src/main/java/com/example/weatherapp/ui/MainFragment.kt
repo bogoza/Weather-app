8ef9d49af3d06a831eb64315ba002646ab9e405e
chach <chachanidze.grg@gmail.com>
2023-06-10 19:00:15 +0400

    before handling tabLayout

--- app/src/main/java/com/example/weatherapp/ui/MainFragment.kt
+++ app/src/main/java/com/example/weatherapp/ui/MainFragment.kt
@@ -6,13 +6,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableLayout
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.FragmentActivity
+import com.example.weatherapp.adapters.VpAdapter
 import com.example.weatherapp.databinding.FragmentMainBinding
 import com.example.weatherapp.isPermissionGranted
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainFragment : Fragment() {
+    private val fList = listOf(
+        HoursFragment.newInstance(),
+        DaysFragment.newInstance()
+    )
+    private val tList = listOf(
+        "Hours",
+        "Days"
+    )
     private lateinit var pLauncher: ActivityResultLauncher<String>
     private var _binding:FragmentMainBinding? = null
     private val binding get() = _binding!!
@@ -27,6 +39,16 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
+        init()
+    }
+
+    private fun init() = with(binding){
+        val adapter = VpAdapter(activity as FragmentActivity,fList)
+        vp.adapter = adapter
+        TabLayoutMediator(tabLayout,vp){
+            tab, pos -> tab.text = tList[pos]
+        }
+
     }
 
     private fun permissionListener(){
@@ -43,6 +65,7 @@ class MainFragment : Fragment() {
     }
     companion object{
         @JvmStatic
-        fun newInstance() = MainFragment()
+       fun newInstance() = MainFragment()
     }
+
 }
